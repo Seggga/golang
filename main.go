bba2d8b031d4a779730a9f2ce0b362fd352b7afb
Serg Kotovsky <info@geekserg.ru>
2021-01-15 18:41:22 +0300

    Установил GO для Windows, текущая версия 1.15.6. Создал go-файл Hello-world.

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+
+3. Написать любое приложение на Go.
+Например, написать приложение, которое выводит на экран “Hello, World!”.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world !")
+}
