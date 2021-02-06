7e984334d0ee73d18602ddae253084f9db391eb9
Seggga <32059926+Seggga@users.noreply.github.com>
2021-02-06 13:18:18 +0300

    Update main.go
    изменил имена переменных на более читабельные
    тип переменных uint

--- task3/main.go
+++ task3/main.go
@@ -1,9 +1,9 @@
+package main
+
 /*
 3. С клавиатуры вводится трехзначное число. Выведите цифры, соответствующие количеству
 сотен, десятков и единиц в этом числе.
 */
-
-package main
 
 import (
 	"fmt"
@@ -12,38 +12,42 @@ import (
 
 func main() {
 
-	var num, ed, des, sot int
+	var userNumber, position0, position1, position2 uint
 	var userInput string
 
-	fmt.Println("Программа вычисляет значения разрядов трехзначного десятичного числа, введенного пользователем.")
-	fmt.Printf("введите трехзначное число : ")
+	fmt.Println("Application takes a 3-digit positive number and calculates entered digits.")
+	fmt.Printf("Please enter 3-digit number : ")
 	fmt.Scanln(&userInput)
 
 	// перевод введенных данных в число
-	num, err := strconv.Atoi(userInput)
+	userNumber, err := strconv.Atoi(userInput)
 
 	// проверка на наличие ошибки
 	if err != nil {
-		fmt.Printf("введенные данные не удается перевести в число.")		
-	} else {
-		fmt.Printf("число %d содержит:\n", num)
-
-		// проверка знака введенного числа
-		if num < 0 {
-			num *= (-1)
-		}
-	
-		// вычисление единиц, десятков и сотен 
-		ed  = num % 10
-		num = num / 10
-		des = num % 10
-		sot = num / 10
-	
-		// вывод результата
-		fmt.Printf("- сотен    %d\n", sot)
-		fmt.Printf("- десятков %d\n", des)
-		fmt.Printf("- единиц   %d\n", ed)
-
+		// есть ошибка, выдается сообщение, вычисления не производятся
+		fmt.Println("...entered data cannot be recognized as a nubmer.")
+		fmt.Println(err)
+		return
 	}
+	
+	// проверка на валидность введенных данных
+	if userNumber < 100 || userNumber > 999 {
+		// невалидные введенные данные 
+		fmt.Printf("...entered data is not valid ( not a 3-digit number ).\n")
+		return
+	}
+	
+	fmt.Printf("entered number %d contains:\n", userNumber)
+	
+	// вычисление единиц, десятков и сотен 
+	position0 = num % 10
+	userNumber = userNumber / 10
+	position1 = userNumber % 10
+	position2 = userNumber / 10
+	
+	// вывод результата
+	fmt.Printf("- hundreds  %d\n", position2)
+	fmt.Printf("- tens      %d\n", position1)
+	fmt.Printf("- units     %d\n", position0)
 
 }
